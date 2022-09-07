0cf2f14f9965b4bea918181c65c421ccd9a32340
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-07 11:04:50 -0500

    Losses

--- main.py
+++ main.py
@@ -138,3 +138,30 @@ plt.imshow(discriminator_x(sample_horse)[0, ..., -1], cmap='RdBu_r')
 plt.savefig('figure_4.png')
 print("Model builded")
 
+# Loss function
+LAMBDA = 10
+
+loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real, generated):
+  real_loss = loss_obj(tf.ones_like(real), real)
+
+  generated_loss = loss_obj(tf.zeros_like(generated), generated)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss * 0.5
+
+def generator_loss(generated):
+  return loss_obj(tf.ones_like(generated), generated)
+
+def calc_cycle_loss(real_image, cycled_image):
+  loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
+
+  return LAMBDA * loss1
+
+def identity_loss(real_image, same_image):
+  loss = tf.reduce_mean(tf.abs(real_image - same_image))
+  return LAMBDA * 0.5 * loss
+
+# Optimizers
